83d7d05b464d5c5d909bc1b4026fd9554f629604
shubham6726 <shubham2060@gmail.com>
2017-05-06 15:19:54 +0530

    Add files via upload

---
+++ ScraperVersion2.py
@@ -0,0 +1,56 @@
+
+# Navigate to a webpage
+from selenium import webdriver
+
+
+from selenium.webdriver.common.keys import Keys #Importing keys
+import os
+#print os.getcwd() #to understand the working directory
+
+driver = webdriver.Chrome()
+#Pull Search Terms from a CSV File
+file = open('searchdata.txt', 'r')
+while True:
+	srchtxt = file.readline()
+	if not srchtxt: break
+	driver.get('http://www.justdial.com/') #opening justdial/patna
+
+	search_box = driver.find_element_by_id("srchbx") #findng searchbox
+	search_box.send_keys(srchtxt, Keys.RETURN) #sending text and Enter
+	driver.execute_script("window.scrollTo(0, document.body.scrollHeight);") #to scroll to page bottom
+#BROWSER WAIT to be included
+
+text_file = open('urllist.txt', 'a') #open text file to copy all urls
+#using append instead of write
+#content = driver.find_element_by_class_name('cntanr')
+elems = driver.find_elements_by_xpath("//a[@href]")
+for elem in elems:
+    #print elem.get_attribute("href")
+    text_file.write(elem.get_attribute("href") + "\n")
+clss = driver.find_elements_by_class_name('cntanr')
+for cls in clss:
+	text_file.write(cls.text + "\n")
+
+text_file.close()
+
+
+
+file.close()
+
+
+
+#Below every search term, Put the feed in a coloumn
+
+
+
+#Pick each item in each coloumn and find phone number
+
+
+
+#Write the phone number in CSV file
+
+
+
+
+#close browser
+driver.close()
